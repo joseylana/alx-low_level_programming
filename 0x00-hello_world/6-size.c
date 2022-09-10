2bc72b1fd228d3a2af3e343498e4338b47a6e7b6
joseylana <joseylana@github.com>
2022-09-09 18:59:16 -0700

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- *main - This prints size of datatypes
- *return - 0 if successful 
+ *main - Prints out the sizes of various data types on the computer
+ *this program is compiled on.
+ *Return: 0 if success.
  */
 int main(void)
 {
