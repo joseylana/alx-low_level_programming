315c75de52f0d0fd1fecf8de8de5db5e3f13cd09
joseylana <joseylana@github.com>
2022-09-09 18:46:12 -0700

    u

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,6 @@
 /**
  *This prints size of datatypes
  */
-
 int main(void)
 {
 int charSize = sizeof(char);
@@ -10,12 +9,10 @@ int intSize = sizeof(int);
 int longSize = sizeof(long int);
 int longlongSize = sizeof(long long int);
 int floatSize = sizeof(float);
-
 printf("Size of a char: %d byte(s)\n", charSize);
 printf("Size of an int: %d byte(s)\n", intSize);
 printf("Size of a long int: %d byte(s)\n", longSize);
 printf("Size of a long long int: %d byte(s)\n", longlongSize);
 printf("Size of a float: %d byte(s)\n", floatSize);
-  
-  return (0);
+return (0);
 }
