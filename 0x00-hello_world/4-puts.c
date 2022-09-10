575e04cff27f95f59c0a5b67cd39bbf447105499
joseylana <joseylana@github.com>
2022-09-09 19:10:10 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-
 /**
- *this prints using puts 
+ *main- This prints using puts 
  *function
  *Betty style is weird
  *
-*/
-
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
