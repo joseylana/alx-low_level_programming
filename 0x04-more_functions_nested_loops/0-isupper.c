33ae1078239fd24ce5daf09801bafbd8dbd4dcf1
joseylana <joseylana@github.com>
2022-09-15 11:33:07 -0700

    nested

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code.
- *
  * Return: Always 0.
  */
 int main(void)
 {
   char c;
-
   c = 'A';
   printf("%c: %d\n", c, _isupper(c));
   c = 'a';
